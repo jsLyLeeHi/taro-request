89205cac43b2af0331628c9461d5df5ffccb8b84
其实 <747452833@qq.com>
2020-07-12 16:53:56 +0800

    改一点参数

--- src/components/UI/modal/index.tsx
+++ src/components/UI/modal/index.tsx
@@ -12,7 +12,7 @@ export default function Index(props: P) {
     // let inAnimate = [{ className: 'bounceInUp', duration: 800 }, { className: 'wobble', duration: 800 }],
     let inAnimate = [{ className: 'bounceInUp', duration: 800 }],
         outAnimate = [{ className: 'bounceOutDown', duration: 0 }]
-    return <UIMask outDuration={1600} isShow={props.isShow} renderModal={() => (
+    return <UIMask outDuration={800} isShow={props.isShow} renderModal={() => (
         <View className='modal-box' onClick={props.onHide}>
             <UIAnimate animate={props.isShow ? inAnimate : outAnimate} renderChilden={() => (
                 <View className='modal'>我是个Modal组件</View>
